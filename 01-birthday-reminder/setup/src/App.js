d7bdb6028b3a593d0c3aab37bdc8d27b0ec3f576
GeoMukkath <geomukkath@yandex.com>
2021-05-31 09:25:39 +0530

    Adding all files

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  return <h2>This is a demo app</h2>;
 }
 
 export default App;
