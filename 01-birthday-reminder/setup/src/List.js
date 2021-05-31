9538234805ba63d0155a29f1d67eb8060e8cd2b8
GeoMukkath <geomukkath@yandex.com>
2021-05-31 11:38:33 +0530

    Completed Birthday reminder

--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const List = () => {
+const List = (props) => {
   return (
     <>
-      <h2>list component</h2>
+      {props.people.map((person) => {
+        return (
+          <article key={person.id} className="person">
+            <img src={person.image} alt={person.name} />
+            <div>
+              <h4>{person.name}</h4>
+              <p>{person.age}</p>
+            </div>
+          </article>
+        );
+      })}
     </>
   );
 };
